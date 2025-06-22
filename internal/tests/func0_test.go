2da1658a12ffe57aa2628678f5549243354a045e
“Naeel” <tazet@narod.ru>
2025-06-22 08:09:57 +0300

    integra

---
+++ internal/tests/func0_test.go
@@ -0,0 +1,22 @@
+//go:build integration
+
+package tests
+
+import (
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCap(t *testing.T) {
+	resp, err := http.Get("http://0.0.0.0:8100/cap")
+	require.NoError(t, err)
+	defer resp.Body.Close()
+	require.EqualValues(t, http.StatusOK, resp.StatusCode)
+
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+	_ = body
+}
